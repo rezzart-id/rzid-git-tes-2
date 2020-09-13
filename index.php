0d47e4424bacc6c571da120deb25f8fa469cbd0d
rezzart-id <rzaditya89@gmail.com>
2020-09-13 14:31:18 +0700

    add lits into index.php

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Ini coba-coba</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque dolor, quaerat eligendi quod magnam sunt libero voluptatum nesciunt possimus! Enim maiores debitis quod animi officiis tempore fugit doloribus labore.</p>
+    <ul>
+        <li>tes1</li>
+        <li>tes2</li>
+    </ul>
 </body>
 
 </html>
